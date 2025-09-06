6e91cdba633aa5a6e78e60bba273a17a71fb5553
Almujeer Uddin <68914647+JamRuinedDuel@users.noreply.github.com>
2025-09-06 23:10:58 +0900

    tinkering

--- neovim-0.11/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim-0.11/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,8 @@
 return {
-  {
-    "thesimonho/kanagawa-paper.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("kanagawa-paper").setup({
-        transparent = true,
-      })
-
-      -- vim.cmd.colorscheme("kanagawa-paper-ink")
-    end,
-  },
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function() end,
-  },
-  {
-    "mcauley-penney/techbase.nvim",
-    config = function()
-      vim.cmd.colorscheme("techbase")
-    end,
-    priority = 1000,
-  },
+  "shaunsingh/nord.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("nord")
+  end,
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,24 +1,35 @@
 local wezterm = require("wezterm")
-local config = {}
+local config = wezterm.config_builder()
 
+-- Font Settings
 config.font = wezterm.font({
 	family = "MesloLGSDZ Nerd Font",
 	weight = "Regular",
 	italic = false,
 })
-config.font_size = 20
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.font_size = 18
 
+config.harfbuzz_features = {
+	"calt=0",
+	"clig=0",
+	"liga=0",
+}
+
+-- Color & Theme Settings
+config.color_scheme = "nord"
+
+-- Tab Settings
 config.enable_tab_bar = false
+
+-- Window Settings
 config.window_decorations = "RESIZE"
 
-config.color_scheme = "kanagawabones"
-
+-- Startup Settings
 config.default_prog = {
 	"/bin/zsh",
 	"-l",
 	"-c",
-	"tmux attach || tmux new -s 0",
+	"tmux attach || tmux new -s terminal",
 }
 
 return config
